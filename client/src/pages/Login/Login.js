a7bccabf96f3acd3a7811540fac6d9a5c7468769
kryptokev <recodedkevdev@gmail.com>
2022-11-29 01:06:48 -0800

    got rid of uneeded div

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -5,20 +5,18 @@ import style from './Login.css.js';
 
 const Login = () => {
     return (
-        <div style={style.centerParent}>
-            <div style={style.centerChild}>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+        <main style={style.centerParent}>
+            <Form style={style.centerChild}>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
 
-                        <Button variant="primary" type="submit">
-                            submit
-                        </Button>
-                    </Form.Group>
-                </Form>
-            </div>
-        </div>
+                    <Button variant="primary" type="submit">
+                        submit
+                    </Button>
+                </Form.Group>
+            </Form>
+        </main>
     );
 };
 
