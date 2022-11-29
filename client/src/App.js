8803d97f464fe0ac472997bcc340e42b35e908d9
kryptokev <recodedkevdev@gmail.com>
2022-11-29 00:16:00 -0800

    implemented React Router and added some test pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,17 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import {Route, Routes} from 'react-router-dom';
 
-function App() {
-  const [show, setShow] = useState(false);
+// Import pages for application
+import Login from './pages/Login.js';
+import Home from './pages/Home.js';
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+const App = () => {
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <div>
+        <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+        </Routes>
+    </div>
   );
 }
 
