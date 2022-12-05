5cf41b1e1241feb9f6ffca5ea45fb19504aca1c4
kryptokev <recodedkevdev@gmail.com>
2022-12-04 20:29:33 -0800

    Finished rough styling for login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,20 +1,54 @@
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import style from './Login.css.js';
 
 const Login = () => {
     return (
         <main style={style.centerParent}>
-            <Form className="d-flex align-items-center justify-content-center">
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+            <Form  className='bg-dark border rounded' style={style.centerChild}>
+                <h1 className='mb-3'>Family Charades</h1>
 
-                    <Button variant="primary" type="submit">
-                        submit
-                    </Button>
-                </Form.Group>
+                <Row>
+                    <FloatingLabel
+                        className="mb-3 p-2 text-dark"
+                        controlId="floatingUsername"
+                        label="First Name"
+                    >
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter First Name"
+                        />
+                    </FloatingLabel>
+                </Row>
+
+                <Row>
+                    <FloatingLabel
+                        className="mb-3 p-2 text-dark"
+                        controlId="floatingPassword"
+                        label="Password"
+                    >
+                        <Form.Control
+                            type="password"
+                            placeholder="Please Enter Password"
+                        />
+                    </FloatingLabel>
+                </Row>
+
+                <Row spacearound>
+                    <Col className=''>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Col>
+                    <Col >
+                        <Button variant="primary" type="submit">
+                            Sign Up
+                        </Button>
+                    </Col>
+                </Row>
             </Form>
         </main>
     );
