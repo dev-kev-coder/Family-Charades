1b006478bb6f307a9b575543ce3d5cd47ddc1491
kryptokev <recodedkevdev@gmail.com>
2022-12-04 20:50:59 -0800

    set up pseudocode for next task to complete

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,13 @@
 const Home = () => {
     return (
+        // To-do: We will need to generate a series of cards.
+        // We will map through a data set that is retrive by and API call
+        // Default value is an empty array
+
+        // We will constantly have a default "Create New Deck Card" that will be 
+
         <>
+            
             <h1 className="text-dark">This is the homepage</h1>
         </>
     )
