ab4ec092f8cbeb9b1a0154f6755247bcf467d962
kryptokev <recodedkevdev@gmail.com>
2022-12-05 19:22:46 -0800

    changes that i will eventually get rid of

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,14 @@ import Login from './pages/Login/Login.js';
 import Home from './pages/Home.js';
 
 const App = () => {
-    return (
-        <Container fluid>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
-        </Container>
-    );
+	return (
+		<Container fluid>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</Container>
+	);
 };
 
 export default App;
