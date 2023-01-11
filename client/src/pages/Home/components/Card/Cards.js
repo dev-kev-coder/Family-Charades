226e99f8d517551bbe9c5dd51d851058b49de6d4
kryptokev <recodedkevdev@gmail.com>
2023-01-10 23:51:22 -0800

    full generating cards based off of data json that is passed as well as assigning onclick handlers to each card

--- client/src/pages/Home/components/Card/Cards.js
+++ client/src/pages/Home/components/Card/Cards.js
@@ -1,26 +1,16 @@
 import Col from 'react-bootstrap/Col';
 
-const Cards = ({ categories }) => {
-	const categoryKeys = Object.keys(categories);
-	const categoryCards = categoryKeys.map((category, index) => {
+const Cards = ({ categories, selectCategory }) => {
+	const categoryNames = Object.keys(categories);
+	const categoryCards = categoryNames.map((category, index) => {
 		return (
 			<Col key={index} lg="3">
-				<div className="category-card">
+				<div onClick={selectCategory} className="category-card">
 					<h1>{category}</h1>
 				</div>
 			</Col>
 		);
 	});
-
-	// return (
-	// 	<>
-	// 		<Col lg="3">
-	// 			<div className="initial-create-card">
-	// 				<h1>Testds</h1>
-	// 			</div>
-	// 		</Col>
-	// 	</>
-	// );
 	return categoryCards;
 };
 
